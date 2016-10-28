46173450549958ea0740287f33ca1bf652f870c4
sergey90x <cheremisin.sergey@yandex.ru>
2016-10-28 17:39:14 +0300

    db

---
+++ src/main/resources/db.migration/V1__dump.sql
@@ -0,0 +1,17 @@
+CREATE TABLE Users (
+  Users_id  int  not null auto_increment,
+  Name varchar(255),
+  Login varchar(255) not null,
+  Email varchar(255) not null,
+  Password varchar(255) not null,
+  PRIMARY KEY(Users_id),
+  UNIQUE (Login),
+  UNIQUE (Email)
+);
+
+CREATE TABLE Sessions(
+  Session_id int not null,
+  User_id int not null,
+  PRIMARY KEY (Session_id),
+  FOREIGN KEY (User_id) REFERENCES Users(User_id)
+);
